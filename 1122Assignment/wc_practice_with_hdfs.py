82fc050469ca81dc6377d181d76ad5555c83981a
Donghoon Han <dhk1349@naver.com>
2019-11-24 16:07:47 +0900

    update

--- 1122Assignment/wc_practice_with_hdfs.py
+++ 1122Assignment/wc_practice_with_hdfs.py
@@ -2,6 +2,7 @@ import sys
 import re
 from operator import add
 from pyspark import SparkContext
+from pyspark.sql import SparkSession
 
 def map_phase(x):
     x = re.sub('--', ' ', x)
@@ -12,8 +13,14 @@ if __name__ == "__main__":
 	if len(sys.argv) < 4:
 		print >> sys.stderr, "Usage: wordcount <master> <inputfile> <outputfile>"
 		exit(-1)
-	sc = SparkContext(sys.argv[1], "python_wordcount_sorted in bigdataprogrammiing")
-	lines = sc.textFile(sys.argv[2],2)
+    
+    sparkSession = SparkSession.builder.appName("test").getOrCreate()
+    data=read(sys.argv[1])
+    df=sparkSession.createDataFrame(data)
+    df.write.csv("hdfs://cluster/user/hdfs/test/example.csv")
+    df_load = sparkSession.read.csv('hdfs://cluster/user/hdfs/test/example.csv')
+	#sc = SparkContext(sys.argv[1], "python_wordcount_sorted in bigdataprogrammiing")
+	lines = sc.textFile(df_load,2)
 	print(lines.getNumPartitions()) # print the number of partitions
    	words=lines.map(lambda x: x.lower())
    	words=words.filter(lambda x: x!='tokyo')
