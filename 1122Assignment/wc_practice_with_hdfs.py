f1d497f51a105f2e638a18745d6210b7446e14f5
dhk1349 <dhk1349@naver.com>
2019-11-24 16:21:35 +0900

    a

--- 1122Assignment/wc_practice_with_hdfs.py
+++ 1122Assignment/wc_practice_with_hdfs.py
@@ -14,11 +14,11 @@ if __name__ == "__main__":
 		print >> sys.stderr, "Usage: wordcount <master> <inputfile> <outputfile>"
 		exit(-1)
     
-    sparkSession = SparkSession.builder.appName("test").getOrCreate()
-    data=read(sys.argv[1])
-    df=sparkSession.createDataFrame(data)
-    df.write.csv("hdfs://cluster/user/hdfs/test/example.csv")
-    df_load = sparkSession.read.csv('hdfs://cluster/user/hdfs/test/example.csv')
+    	sparkSession = SparkSession.builder.appName("test").getOrCreate()
+    	#data=open(sys.argv[2])
+    	df=sparkSession.createDataFrame(sys.argv[2])
+    	df.write.csv("hdfs://cluster/user/hdfs/test/example.csv")
+    	df_load = sparkSession.read.csv('hdfs://cluster/user/hdfs/test/example.csv')
 	#sc = SparkContext(sys.argv[1], "python_wordcount_sorted in bigdataprogrammiing")
 	lines = sc.textFile(df_load,2)
 	print(lines.getNumPartitions()) # print the number of partitions
