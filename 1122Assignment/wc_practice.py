7a8c138f3a0c6c32eb78ea313cd75fcebef45c7f
dhk1349 <dhk1349@naver.com>
2019-11-24 16:00:44 +0900

    update

--- 1122Assignment/wc_practice.py
+++ 1122Assignment/wc_practice.py
@@ -11,15 +11,15 @@ def map_phase(x):
 
 if __name__ == "__main__":
 	if len(sys.argv) < 4:
-        print >> sys.stderr, "Usage: wordcount <master> <inputfile> <outputfile>"
-        exit(-1)
+		print >> sys.stderr, "Usage: wordcount <master> <inputfile> <outputfile>"
+		exit(-1)
 	sc = SparkContext(sys.argv[1], "python_wordcount_sorted in bigdataprogrammiing")
 	lines = sc.textFile(sys.argv[2],2)
 	print(lines.getNumPartitions()) # print the number of partitions
-    words=lines.map(lambda x: x.lower())
-    words=words.filter(lambda x: x!='tokyo')
-    words=words.filter(lambda x: x!='neighborhood')
-    words=words.map(lambda x: (x, 1))
-    result=words.reduceByKey(lambda x,y :x+y)
-    result=result.sortBy(lambda x: x[0])
+   	words=lines.map(lambda x: x.lower())
+   	words=words.filter(lambda x: x!='tokyo')
+    	words=words.filter(lambda x: x!='neighborhood')
+    	words=words.map(lambda x: (x, 1))
+    	result=words.reduceByKey(lambda x,y :x+y)
+    	result=result.sortBy(lambda x: x[0])
 	result.saveAsTextFile(sys.argv[3])
